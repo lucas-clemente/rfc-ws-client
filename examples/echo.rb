da90ebe72977d8b3a3ba26c4e31d64656b46fed8
Lucas Clemente <luke.clemente@gmail.com>
2012-11-19 17:47:40 +0100

    simple send, receive and close

--- examples/echo.rb
+++ examples/echo.rb
@@ -2,4 +2,30 @@ $: << File.dirname(__FILE__) + "/../lib"
 
 require "rfc-ws-client"
 
-ws = RfcWebsocket::Websocket.new("ws://echo.websocket.org")
+# ws = RfcWebsocket::Websocket.new("ws://echo.websocket.org")
+# ws.send_message("blub", binary: true)
+# puts "sent blub"
+# answer = ws.receive[0]
+# puts "answer: #{answer}"
+# ws.close
+
+ws = RfcWebsocket::Websocket.new("ws://localhost:9001/getCaseCount")
+count = ws.receive()[0].to_i
+ws.close
+
+(1..count).each do |i|
+  puts "#{i}/#{count}"
+  begin
+    ws = RfcWebsocket::Websocket.new "ws://localhost:9001/runCase?&case=#{i}&agent=rfc-ws-client"
+    while true
+      data, binary = ws.receive
+      break if binary.nil?
+      ws.send_message data, binary: binary
+    end
+  rescue
+  end
+end
+
+puts "Updating reports and shutting down"
+ws = RfcWebsocket::Websocket.new "ws://localhost:9001/updateReports?agent=rfc-ws-client"
+ws.receive
--- lib/rfc-ws-client.rb
+++ lib/rfc-ws-client.rb
@@ -11,17 +11,16 @@ require 'base64'
 module RfcWebsocket
   class Websocket
     WEB_SOCKET_GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
-    OPCODE_CONTINUATION = 0x00
     OPCODE_TEXT = 0x01
     OPCODE_BINARY = 0x02
     OPCODE_CLOSE = 0x08
     OPCODE_PING = 0x09
     OPCODE_PONG = 0x0a
+    DEBUG = true
 
     def initialize(uri, protocol = "")
       uri = URI.parse(uri) unless uri.is_a?(URI)
       @protocol = protocol
-      path = (uri.path.empty? ? "/" : uri.path) + (uri.query ? "?" + uri.query : "")
 
       if uri.scheme == "ws"
         default_port = 80
@@ -30,6 +29,8 @@ module RfcWebsocket
       else
         raise "unsupported scheme: #{uri.scheme}"
       end
+      host = uri.host + ((!uri.port || uri.port == default_port) ? "" : ":#{uri.port}")
+      path = (uri.path.empty? ? "/" : uri.path) + (uri.query ? "?" + uri.query : "")
 
       @socket = TCPSocket.new(uri.host, uri.port || default_port)
       if uri.scheme == "wss"
@@ -39,11 +40,11 @@ module RfcWebsocket
       end
 
       request_key = SecureRandom::base64(16)
-      write(handshake(uri.host, path, request_key))
+      write(handshake(host, path, request_key))
       flush()
 
       status_line = gets.chomp
-      raise "bad response: #{line}" unless status_line.start_with?("HTTP/1.1 101")
+      raise "bad response: #{status_line}" unless status_line.start_with?("HTTP/1.1 101")
 
       header = {}
       while line = gets
@@ -60,26 +61,78 @@ module RfcWebsocket
       raise "sec-websocket-accept missing" unless accept
       expected_accept = Digest::SHA1.base64digest(request_key + WEB_SOCKET_GUID)
       raise "sec-websocket-accept is invalid, actual: #{accept}, expected: #{expected_accept}" unless accept == expected_accept
-
-      @buffer = ""
-      
     end
 
-    def send_message
-      
+    def send_message(message, opts = {binary: false})
+      write(encode(message, opts[:binary] ? OPCODE_BINARY : OPCODE_TEXT))
+    end
+
+    def receive
+      begin
+        bytes = read(2).unpack("C*")
+        fin = (bytes[0] & 0x80) != 0
+        opcode = bytes[0] & 0x0f
+        mask = (bytes[1] & 0x80) != 0
+        plength = bytes[1] & 0x7f
+        if plength == 126
+          bytes = read(2)
+          plength = bytes.unpack("n")[0]
+        elsif plength == 127
+          bytes = read(8)
+          (high, low) = bytes.unpack("NN")
+          plength = high * (2 ** 32) + low
+        end
+        mask_key = mask ? read(4).unpack("C*") : nil
+        payload = read(plength)
+        payload = apply_mask(payload, mask_key) if mask
+        case opcode
+        when OPCODE_TEXT
+          return payload.force_encoding("UTF-8"), false
+        when OPCODE_BINARY
+          return payload, true
+        when OPCODE_CLOSE
+          close
+          return nil, nil
+        when OPCODE_PING
+          write(encode(payload, OPCODE_PONG))
+          #TODO fix recursion
+          return receive
+        when OPCODE_PONG
+        else
+          raise "received unknown opcode: #{opcode}"
+        end
+      rescue EOFError
+        return nil, nil
+      end
+    end
+
+    def close(code = 1000, msg = nil)
+      write(encode [code ? code : 1000, msg].pack("nA*"), OPCODE_CLOSE)
+      @socket.close
     end
 
     private
 
     def gets(delim = $/)
       line = @socket.gets(delim)
-      print line.color(:green)
+      print line.color(:green) if DEBUG
       line
     end
 
     def write(data)
-      print data.color(:yellow)
+      print data.color(:yellow) if DEBUG
       @socket.write(data)
+      @socket.flush
+    end
+
+    def read(num_bytes)
+      str = @socket.read(num_bytes)
+      if str && str.bytesize == num_bytes
+        print str.color(:green) if DEBUG
+        str
+      else
+        raise(EOFError)
+      end
     end
 
     def flush
@@ -96,6 +149,48 @@ module RfcWebsocket
       headers << "User-Agent: rfc-ws-client"
       headers << "\r\n"
       headers.join "\r\n"
+    end
+
+    def encode(data, opcode)
+      frame = []
+      frame << (opcode | 0x80)
+
+      packr = "CC"
+
+      if opcode == OPCODE_TEXT
+        data.force_encoding("UTF-8")
+        if !data.valid_encoding?
+          raise "Invalid UTF!"
+        end
+      end
+
+      # append frame length and mask bit 0x80
+      len = data ? data.bytesize : 0
+      if len <= 125
+        frame << (len | 0x80)
+      elsif len < 65536
+        frame << (126 | 0x80)
+        frame << len
+        packr << "n"
+      else
+        frame << (127 | 0x80)
+        frame << len
+        packr << "L!>"
+      end
+
+      # generate a masking key
+      key = rand(2 ** 31)
+
+      # mask each byte with the key
+      frame << key
+      packr << "N"
+
+      # Apply the masking key to every byte
+      len.times do |i|
+        frame << ((data.getbyte(i) ^ (key >> ((3 - (i % 4)) * 8))) & 0xFF)
+      end
+
+      frame.pack("#{packr}C*")
     end
   end
 end
